9ae3b26c9be513a7148aeb8bccd64e48ee55092d
JuanRod <juanrodriguez1028@gmail.com>
2020-11-03 19:08:24 -0500

    v1.Front.2

--- js/register.js
+++ js/register.js
@@ -1,9 +1,9 @@
 $("#Registrar").click(function(e){
     e.preventDefault();
-    var username=$("#username").val();
+    var username=$("#usr").val();
     var pass=$("#pass").val();
         var obj={
-            username: username,
+            username: usr,
             contra: pass
         };
         registrarUsuario(obj)
